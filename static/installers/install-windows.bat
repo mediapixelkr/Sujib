c581144105a6605a59814c1f903cf90e445f7ab1
Mickael <mseraphin@gmail.com>
2026-08-16 12:23:36 +0900

    fix(installer): register machine GPO registry keys alongside policies.json

--- static/installers/install-windows.bat
+++ static/installers/install-windows.bat
@@ -36,7 +36,7 @@ if not exist "!FX_PATH!\distribution\extensions" mkdir "!FX_PATH!\distribution\e
 echo Telechargement du paquet extension dans distribution...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'http://192.168.200.15/sujib/static/sujib-firefox-extension.xpi' -OutFile '!FX_PATH!\distribution\extensions\sujib-downloader@mediapixel.kr.xpi' -UseBasicParsing"
 
-echo Configuration de la politique force_installed dans policies.json...
+echo Configuration du fichier distribution\policies.json...
 (
   echo {
   echo   "policies": {
@@ -50,14 +50,17 @@ echo Configuration de la politique force_installed dans policies.json...
   echo }
 ) > "!FX_PATH!\distribution\policies.json"
 
+echo Inscription dans le Registre Windows (Politiques d'entreprise)...
+reg add "HKLM\SOFTWARE\Policies\Mozilla\Firefox\ExtensionSettings\sujib-downloader@mediapixel.kr" /v installation_mode /t REG_SZ /d "force_installed" /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Policies\Mozilla\Firefox\ExtensionSettings\sujib-downloader@mediapixel.kr" /v install_url /t REG_SZ /d "http://192.168.200.15/sujib/static/sujib-firefox-extension.xpi" /f >nul 2>&1
+
 echo.
 echo ===================================================
 echo [SUCCES] L'extension Sujib a ete installee avec succes !
 echo.
 echo IMPORTANT :
-echo 1. Fermez completement toutes les fenetres de Firefox puis rouvrez-le.
-echo 2. Dans Firefox, verifiez l'icone de puzzle (Extensions) en haut a droite
-echo    puis cliquez sur la roue crantee pour "Epingler a la barre d'outils".
+echo 1. Fermez completement TOUTES les fenetres de Firefox.
+echo 2. Relancez Firefox. L'extension sera activee par politique.
 echo ===================================================
 echo.
 pause
